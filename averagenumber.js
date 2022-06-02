37c60a0b969b60b96b2f4b1fa41fcd75c317a699
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-02 16:06:58 +0530

    Add files via upload

---
+++ averagenumber.js
@@ -0,0 +1,7 @@
+let tamil = 80, english = 50, maths = 35, science = 75, social = 75
+
+let totalval = tamil + english + maths + science + social 
+
+let average = totalval / 5
+
+console.log('Total = ',totalval, 'Average =', average)
